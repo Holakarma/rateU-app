97400206f0e2d7116cf7bfbd7a6b2a24062e803c
AnastasiaManaeva <tvoyasovushka@yandex.ru>
2024-03-20 15:24:19 +0500

    uiuiuiuiuiui

--- src/components/mainApp-interface/UsersSection/UserSection.jsx
+++ src/components/mainApp-interface/UsersSection/UserSection.jsx
@@ -1,11 +1,20 @@
 import React from 'react';
 import { UserSelect } from './UserSelect/UserSelect';
+import { ShowEmployee } from './/ShowEmployee/ShowEmployee';
 
 export function UserSection() {
+    const [employees, setEmployees] = React.useState([])
+    console.log(employees);
+
     return (
         <div>
             <h3>Оценка сотрудников</h3>
-            <UserSelect />
+            <UserSelect setEmployees={setEmployees}/>
+            <div className='mt-4 row g-3'>
+                    {employees.map((employee) =>
+                        <ShowEmployee employee={employee}/>
+                    )}
+            </div>
         </div>
     );
-}
+} 
