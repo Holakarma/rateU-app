277dcb0826d664b59cd670e2c59d886bc2db6363
Holakarma <holakarmos@gmail.com>
2024-03-26 16:25:11 +0500

    added rates calculations

--- src/components/mainApp-interface/UsersSection/ShowEmployee/ShowEmployee.jsx
+++ src/components/mainApp-interface/UsersSection/ShowEmployee/ShowEmployee.jsx
@@ -1,36 +1,67 @@
 import React from 'react';
+import { ShowCriterionRate } from '../ShowCriterionRate/ShowCriterionRate';
 // import cls from './showEmployee.module.css'
 
-export function ShowEmployee({employee, criteria}) {
+export function ShowEmployee({ employee, criteria, fetchedRates }) {
     const domain = window.location.hostname;
-    console.log(criteria)
 
-    return  (
-        <div className='col-6'>
-            <div className='card mt-2 position-relative h-100'>
-                <div className='card-body row'>
-                    <div className='col-2'>
-                        <img className='rounded-circle w-100' src={`https://avtorit24.ru${employee.photo}`}/>
+    const [sumRates, setSumRates] = React.useState(0);
+    const sum = React.useRef(0);
+    const [countRates, setCountRates] = React.useState(0);
+
+    return (
+        <div className="col-6">
+            <div className="card mt-2 position-relative h-100">
+                <div className="card-body row">
+                    <div className="col-2">
+                        <img
+                            className="rounded-circle w-100"
+                            src={`https://avtorit24.ru${employee.photo}`}
+                        />
                         {/* <img className='rounded-circle w-100' src={`https://${domain}${employee.photo}`}/> RELEASE */}
                     </div>
-                    <div className='col-7 ps-0'>
+                    <div className="col-7 ps-0">
                         <h4>{employee.name}</h4>
-                        <span>Средний балл: 0/10</span>
+                        <span>
+                            Средний балл:{' '}
+                            {countRates ? (
+                                <span>
+                                    {Math.round(
+                                        (sumRates * 100) / (countRates + 1),
+                                    ) / 100}
+                                    <span className='opacity-50'>/10</span>
+                                </span>
+                            ) : (
+                                <i className='opacity-50'>Нет оценок</i>
+                            )}
+                        </span>
                     </div>
-                    <div className='col-3'> 
-                        <button className='text-light rounded border border-primary bg-primary px-2 py-1'>История</button>
+                    <div className="col-3">
+                        <button className="text-light rounded border border-primary bg-primary px-2 py-1">
+                            История
+                        </button>
                     </div>
-                    <div className='px-2 m-0 mt-2'>
-                        <div className='border rounded border-secondary-subtle'>
+                    <div className="px-2 m-0 mt-2">
+                        <div className="border rounded border-secondary-subtle">
                             <ul className="list-group list-group-flush gy-1">
-                                {criteria.map(criterion => 
-                                    <li key={criteria.ID} className="list-group-item">{criterion.NAME}: 10/10</li>
-                                )}
+                                {criteria.map((criterion) => (
+                                    <ShowCriterionRate
+                                        key={criterion.ID}
+                                        fetchedRates={fetchedRates}
+                                        criterion={criterion}
+                                        employeeId={employee.id}
+                                        getRate={(rate) => {
+                                            sum.current += rate;
+                                            setSumRates(sum.current);
+                                            setCountRates(countRates + 1);
+                                        }}
+                                    />
+                                ))}
                             </ul>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    ) 
+    );
 }
--- src/components/mainApp-interface/UsersSection/UserSection.jsx
+++ src/components/mainApp-interface/UsersSection/UserSection.jsx
@@ -1,33 +1,49 @@
 import React, { useEffect } from 'react';
 import { UserSelect } from './UserSelect/UserSelect';
 import { ShowEmployee } from './/ShowEmployee/ShowEmployee';
-import { getCriteria } from '../../../utils/getCriteria'
+import { getCriteria } from '../../../utils/getCriteria';
 import { ChooseCriteria } from './ChooseCriteria/ChooseCriteria';
+import { getRates } from '../../../utils/getRates';
 
 export function UserSection() {
-    const [employees, setEmployees] = React.useState([])
-    const [criteria, setCriteria] = React.useState([])
+    const [employees, setEmployees] = React.useState([]);
+    const [criteria, setCriteria] = React.useState([]);
+    const [fetchedRates, setFetchedRates] = React.useState([]);
 
     useEffect(async () => {
         setCriteria(await getCriteria());
-    }, [])
+        setFetchedRates(await getRates());
+    }, []);
 
     return (
         <div>
             <h3>Оценка сотрудников</h3>
-            <div className='d-flex gap-3'>
-                <UserSelect setEmployees={setEmployees}/>
-                <ChooseCriteria criteria={criteria} setCriteria={setCriteria}/>
+            <div className="d-flex gap-3">
+                <UserSelect setEmployees={setEmployees} />
+                <ChooseCriteria
+                    criteria={criteria}
+                    setCriteria={setCriteria}
+                />
             </div>
-            <div className='mt-4 row g-3'>
-                {employees.map((employee) =>
-                    <ShowEmployee 
-                        key={employee.id} 
-                        employee={employee} 
-                        criteria={criteria}
-                    />
+            <div className="mt-4 row g-3">
+                {fetchedRates.length ? (
+                    employees.map((employee) => (
+                        <ShowEmployee
+                            key={employee.id}
+                            employee={employee}
+                            criteria={criteria}
+                            fetchedRates={fetchedRates}
+                        />
+                    ))
+                ) : (
+                    <div
+                        className="spinner-grow"
+                        role="status"
+                    >
+                        <span className="visually-hidden">Loading...</span>
+                    </div>
                 )}
             </div>
         </div>
     );
-} 
+}
