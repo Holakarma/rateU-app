e7aed5f9035ae39f8192b47b1f8bb3e0432df7cc
AnastasiaManaeva <tvoyasovushka@yandex.ru>
2024-03-28 16:39:57 +0500

    renderCriteria

--- src/components/mainApp-interface/UsersSection/ChooseCriteria/renderCriteria.jsx
+++ src/components/mainApp-interface/UsersSection/ChooseCriteria/renderCriteria.jsx
@@ -1,32 +1,26 @@
 import React from 'react';
+import { Dropdown } from 'react-bootstrap';
+import { FormCheck } from './FormCheck';
+import cls from './renderCriteria.module.css'
 
 export function RenderCriteria({ criteria }) {
 
-    console.log(criteria)
-
-    // const [checkboxCriterion, setCheckboxCriterion] = React.useState(false)
-
-    // function checkboxToggleCriterion() {
-    //     setCheckboxCriterion(!checkboxCriterion)
-    // }
-
     return (
-        <ul className="dropdown-menu rounded">
-            {criteria.map((criterion) => (
-                <li key={criterion.ID} className="dropdown-item">
-                    <input
-                        className="form-check-input me-2 pe-auto"
-                        type="checkbox"
-                        id={`${criterion.ID}dropdownCheckbox`}
-                        onChange={checkboxToggleCriterion}
-                        checked={checkboxCriterion}
-                    />
-                    <label htmlFor={`${criterion.ID}dropdownCheckbox`}>
-                        {criterion.NAME}
-                    </label>
-                </li>
-            )
+        <Dropdown.Menu>
+
+            {criteria.map((criterion) =>
+                <Dropdown.Item criterion={criterion} key={criterion.ID} as={FormCheck} />
             )}
-        </ul>
+
+            <Dropdown.Item key='buttonSaveListCriteria' className={`${cls.criteriaDropdownBtn} d-flex justify-content-center`}>
+                <button
+                    type='button'
+                    className={`btn bg-success-subtle hover-overlay mask ${cls.bgBtn} border border-opacity-50 text-light`}
+                >
+                    Сохранить
+                </button>
+            </Dropdown.Item>
+
+        </Dropdown.Menu>
     )
 }
