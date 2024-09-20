b6bc421efd4df0ab149f967d4e84135547be970f
Holakarma <holakarmos@gmail.com>
2024-09-20 14:22:28 +0500

    added rates table

---
+++ src/components/mainApp-interface/UsersSection/RatesTable/RatesTable.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import cls from './RatesTable.module.css'
+
+const RatesTable = ({employees, selectedCriteria}) => {
+
+    console.log(employees, selectedCriteria);
+
+    if (!employees.length || !selectedCriteria.length) return null;
+
+    return (
+        <table className={`${cls.table} table table-striped`}>
+            <thead>
+            <tr>
+                <th scope="col">Имя</th>
+                {
+                    selectedCriteria.map(criterion => (
+                        <th scope="col">{criterion.NAME}</th>
+
+                    ))
+                }
+            </tr>
+            </thead>
+            <tbody>
+            {employees.map(employee => (
+                <tr>
+                    <th scope="row">{employee.name}</th>
+                    {
+                        Array.from({length: selectedCriteria.length}).map(() => (
+                            <td>9.3</td>
+                        ))
+                    }
+                </tr>
+            ))}
+            </tbody>
+        </table>
+    );
+};
+
+export default RatesTable;
